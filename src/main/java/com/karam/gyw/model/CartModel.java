85a08398c4701e9a6b5117fd3764c95ae9435da2
karamjit-s <imkaramjitsingh@gmail.com>
2022-01-01 18:47:26 +0530

    ui changes

--- src/main/java/com/karam/gyw/model/CartModel.java
+++ src/main/java/com/karam/gyw/model/CartModel.java
@@ -31,6 +31,30 @@ public class CartModel {
 		
 		@Column(name = "price")
 		private String price;
+
+		public long getProduct_id() {
+			return product_id;
+		}
+
+		public void setProduct_id(long product_id) {
+			this.product_id = product_id;
+		}
+
+		public String getProduct_name() {
+			return product_name;
+		}
+
+		public void setProduct_name(String product_name) {
+			this.product_name = product_name;
+		}
+
+		public String getPrice() {
+			return price;
+		}
+
+		public void setPrice(String price) {
+			this.price = price;
+		}
 		
 		
 		
